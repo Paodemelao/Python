694d13e939b8907c926a1eeb1dd7ef46fac21e41
Ana Bea <ana.santos.ribeiro01@escola.pr.gov.br>
2025-05-09 09:41:44 -0300

    exemplo4deoperacoes

---
+++ pythonProject1/capitulo2/exemplo4deoperacoes.py
@@ -0,0 +1,16 @@
+# A maior prioridade são os parênteses
+# depois = exponenciação / potencia
+# depois = multiplicação e divisão
+# soma = subtração na ordem que vier
+# exemplo
+num1 = 76
+num2 = 5
+num3 = 8
+num4 = 4
+num5 = 10
+num6 = 3
+num7 = 2
+
+total = num1 - (num2 + num3 / num4) * (num5 - num6 * num7)
+
+print(total)
