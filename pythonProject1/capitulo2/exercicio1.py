a73b6b3c156df349fd27b98b137edcc38efb5e76
Ana Bea <ana.santos.ribeiro01@escola.pr.gov.br>
2025-05-09 12:17:07 -0300

    exercicio2

---
+++ pythonProject1/capitulo2/exercicio1.py
@@ -0,0 +1,9 @@
+primeirotermo = 4/3
+pi = 3.14
+raio = 5 ** 3
+
+total = primeirotermo * pi * raio
+# total = ((4/3) * 3.14 * (5 ** 1)) ese fui eu q fiz e eo mais dificil
+
+print(total)
+
---
+++ pythonProject1/capitulo2/exercicio2.py
@@ -0,0 +1,10 @@
+capalivro = 24.95
+desconto = capalivro * 0.40
+desconto = capalivro - desconto
+
+freterestanteexemplar = 3.00
+freterestanteexemplares = 0.75
+
+custoatacadoprimeiroexemplar = capalivro + freterestanteexemplar
+custoatacadoprimeiroexemplar + ((capalivro + freterestanteexemplar))
+print('o custo total de atacado de 60 copias é de :',custoatacadoprimeiroexemplar)
